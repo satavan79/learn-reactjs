b70f97d9d3fe2b4a1426cbf3c0940f7a8bfd36c9
TranvanCo <cotran@errpcloud.vn>
2022-07-02 16:45:52 +0700

    Update title 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tran Van Co
+          Xin chào lớp hocj reactjs
         </p>
        
       </header>
